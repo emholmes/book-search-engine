7f9b1eed0ce2f2deb054027c7445432c956fddf9
Erin Holmes <e.holmes09@yahoo.com>
2021-09-29 21:47:35 -0500

    update SearchBooks.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: '/graphql',
