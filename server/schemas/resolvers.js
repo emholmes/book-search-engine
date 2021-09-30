353659727c34846ea0d7e83967d67e4da3417041
Erin Holmes <e.holmes09@yahoo.com>
2021-09-29 20:32:18 -0500

    update saveBook mutation in resolvers to use bookData input

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,11 +40,11 @@ const resolvers = {
       return { token, user };
     },
 
-    saveBook: async (parent, args, context) => {
+    saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { savedBooks: args } },
+          { $addToSet: { savedBooks: bookData } },
           { new: true, runValidators: true }
         );
 
