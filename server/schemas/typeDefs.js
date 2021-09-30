44aeeb4e47932d273609fc473e34e2e9106b470a
Erin Holmes <e.holmes09@yahoo.com>
2021-09-29 20:31:07 -0500

    add input type for saving book, make all params required for mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,6 +18,15 @@ const typeDefs = gql`
     link: String
   }
 
+  input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
   type Auth {
     token: ID!
     user: User
@@ -30,8 +39,8 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: [String], description: String, title: String, bookId: ID, image: String, link: String): User
-    removeBook(bookId: ID): User
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
