349a4733873b2bd51e96d8ccea6c58cd05c34191
Erin Holmes <e.holmes09@yahoo.com>
2021-10-03 16:04:20 -0500

    add google books link to saved books display

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -58,6 +58,7 @@ const SavedBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
+                  <Card.Link href={book.link} target="_blank">View on Google Books</Card.Link>
                   <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book!
                   </Button>
