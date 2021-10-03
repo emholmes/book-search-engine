148c2ce5f8fbdfc1be7fc66ce7010a571380b902
Erin Holmes <e.holmes09@yahoo.com>
2021-10-03 13:39:18 -0500

    add google book link to book results, remove routes and controller files

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -46,6 +46,7 @@ const SearchBooks = () => {
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
+        link: book.volumeInfo.previewLink
       }));
 
       setSearchedBooks(bookData);
@@ -125,6 +126,7 @@ const SearchBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
+                  <Card.Link href={book.link} target="_blank">View on Google Books</Card.Link>
                   {Auth.loggedIn() && (
                     <Button
                       disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
